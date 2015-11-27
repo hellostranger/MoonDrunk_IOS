bf88bfadbf3945c4a5bd660c36c6c9ec8df850a2
povisagas <dkdleltkrktm@gmail.com>
2015-11-27 11:57:48 +0900

    iOS 새로운 버

---
+++ Assets/Scripts/CheckInteraction.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckInteraction : MonoBehaviour {
+	public MediaPlayerCtrl mpc;
+	public Animation anim;
+
+	public static bool stateMoon = false;
+	public static bool stateShadow = false;
+	public static bool stateCup = false;
+	// Use this for initialization
+	void Start () {
+		stateMoon = false;
+		stateShadow = false;
+		stateCup = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mpc.m_CurrentState == MediaPlayerCtrl.MEDIAPLAYER_STATE.PLAYING) {
+			mpc.Pause();
+		}
+		if (stateMoon && stateShadow && stateCup) 
+		{
+			//Animation anim = GetComponent<Animation>();
+			if ( !anim.isPlaying )
+			{
+				Debug.Log ("ani complete");
+				stateMoon = false;
+				stateShadow = false;
+				stateCup = false;
+				//mpc.Play();
+				Application.LoadLevel("Story3");
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/StaticVars.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticVars :MonoBehaviour  {
+
+	private static StaticVars instance;
+	private static bool soundType = false;
+	private AudioSource asc;
+
+	public static StaticVars GetInstance
+	{
+		get
+		{
+			if ( instance == null )
+			{
+				instance = new GameObject("StaticVars").AddComponent<StaticVars>();
+			}
+
+			return instance;
+		}
+	}
+	public void OnApplicationQuit()
+	{
+		instance = null;
+	}
+	void Start()
+	{
+		Debug.Log ("start");
+		instance = this;
+		DontDestroyOnLoad (this);
+	}
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("selectedStroy :" + selectedStroy);
+	}
+
+	public void SoundOn()
+	{
+		Debug.Log ("SoundOn");
+		if (!asc) {
+			asc = gameObject.AddComponent<AudioSource> ();
+			asc.loop = true;
+		}else{
+			asc.Stop();
+		}
+		if ( soundType )
+		{
+			asc.clip = Resources.Load ("narration") as AudioClip;
+		}else{
+			asc.clip = Resources.Load ("letitbe") as AudioClip;
+		}
+		soundType = !soundType;
+		asc.Play ();
+	}
+
+	private int selectedStroy;
+	private int selectedScreen;
+	
+	public int SelectStory
+	{
+		set{ selectedStroy = value; } 
+		get { return selectedStroy;		}
+	}
+	public int SelectScreen
+	{
+		set{ selectedScreen = value; } 
+		get { return selectedScreen;		}
+	}
+}
